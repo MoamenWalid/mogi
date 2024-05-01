fbf2fa3f76e5898949b2e478a68d934f86dfc5db
MoamenWalid <momenelnager@gmail.com>
2024-05-02 01:26:44 +0300

    Changed files in last commit: ['commands.js', 'index.js'] in 1175586470 branch

---
+++ test.js
@@ -0,0 +1,12 @@
+import simpleGit from 'simple-git';
+
+const git = new simpleGit();
+
+// Execute the 'git status' command and get the raw output
+git.raw('checkout -b "wow"')
+  .then(result => {
+    console.log('Raw output of git status:', result);
+  })
+  .catch(err => {
+    console.error('Error executing git status:', err);
+  });
