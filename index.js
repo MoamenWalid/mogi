92b30eef3726735d76900536da70aca131f46db9
MoamenWalid <momenelnager@gmail.com>
2024-05-03 08:39:35 +0300

    Changed files in last commit: ['index.js'] in lgtvkax9 branch

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 import random from 'random-string-generator';
-import { getFilesToCommit } from './fileChanges.js';
-import { program } from 'commander';
-import { exec } from 'node:child_process';
-import { gitCommand } from './commands.js';
 import { simpleGit } from 'simple-git';
+import { exec } from 'node:child_process';
+import { program } from 'commander';
+import { getFilesToCommit } from './fileChanges.js';
+import { gitCommand } from './commands.js';
 
 const git = simpleGit();
 
-// Function if branch isn't exist
+// If branch isn't exist
 const randBranch = (obj) => obj.branch = random(8, 'lowernumeric');
+
+// To get main branch
+const getBaseBranch = git.branch(async (err, { all: branches }) => {
+  if (err) {
+    console.log('Error Branch ❌:', err);
+    return;
+  }
+
+  return await branches.find(branch => branch === 'main' || branch === 'master');
+})
+
+// All actions
+const allActions = async (obj) => {
+  if (!obj.branch) randBranch(obj);
+  if (!obj.message) await getFilesToCommit(obj);
+
+  const baseBranch = await getBaseBranch;
+  const commands = gitCommand(baseBranch, obj.branch, obj.message);
+
+  commands.forEach(command => {
+    exec(command, (err, stdout) => {
+      if (err) {
+        console.error('Error Happen ❌:', err);
+        return;
+      }
+
+      if (stdout) console.log('Success ✅', stdout);
+    });
+  })
+}
 
 program.name('mogi')
   .description('GitHub Desktop simplifies Git and GitHub tasks by offering an intuitive interface, making it easy to upload all data without the need for manual upload commands.')
@@ -18,30 +48,6 @@ program.command('up')
   .description('This base command to mogi start')
   .option('-b, --branch <branch>', 'name of branch')
   .option('-m, --message <message>', 'message when commit')
-  .action(async (obj) => {
-    if (!obj.branch) randBranch(obj);
-    if (!obj.message) await getFilesToCommit(obj);
-
-    git.branch((err, { all: branches }) => {
-      if (err) {
-        console.log('Error ', err);
-        return;
-      }
-
-      const mainBranch = branches.find(branch => branch === 'main' || branch === 'master');
-      const commands = gitCommand(mainBranch, obj.branch, obj.message);
-
-      commands.forEach(command => {
-        exec(command, (err, stdout, stderr) => {
-          if (err) {
-            console.error('Error Happen ❌:', err);
-            return;
-          }
-
-          if (stdout) console.log('Success ✅', stdout);
-        });
-      })
-    })
-  });
+  .action((obj) => allActions(obj));
 
 program.parse();
