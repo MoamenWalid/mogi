9c7a58cad1b956f32d1966e208fd73b8ce27b558
MoamenWalid <momenelnager@gmail.com>
2024-05-05 00:00:20 +0300

    hhh

--- test.js
+++ test.js
@@ -3,14 +3,6 @@ import simpleGit from 'simple-git';
 const git = simpleGit();
 
 git.fetch('--dry-run', (err, fetchResult) => {
-  if (err) {
-    console.error('Error fetching remote changes:', err);
-    return;
-  }
-
-  if (fetchResult && fetchResult.remote && fetchResult.remote.default && fetchResult.remote.default !== 'up to date') {
-    console.log('There are new changes to pull from the remote repository.');
-  } else {
-    console.log('No new changes to pull from the remote repository.');
-  }
+  console.log(err);
+  console.log(fetchResult);
 });
