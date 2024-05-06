e599af8d3b2e28774daa7911754b6caf3d48ac65
MoamenWalid <momenelnager@gmail.com>
2024-05-06 17:10:03 +0300

    Changed files which need to commit: [index.js] in 079ujaac branch

--- index.js
+++ index.js
@@ -65,7 +65,6 @@ program.command('up')
         for (const command of commands.inAll) {
           exec(command, (err, stdout) => {
             if (stdout) console.log('Succes to merge data ✅', stdout);
-            
           });
         }
       }
