a20b7498037297a11fa588eb4bc36248e8556a12
MoamenWalid <momenelnager@gmail.com>
2024-05-06 16:07:41 +0300

    Changed files which need to commit: [index.js] in qn8z1502 branch

--- index.js
+++ index.js
@@ -40,26 +40,25 @@ program.command('up')
         }
       }
 
-      // await new Promise((resolve, reject) => {
-      //   git.fetch(async (err) => {
-      //     if (err) {
-      //       reject(err);
-      //       return;
-      //     }
-      //     const data = await git.diff(['HEAD', 'origin/main']);
-      //     if (data) {
-      //       console.log('Changes detected. Pull is possible.');
-      //       for (const command of commands.inNeedPull) {
-      //         exec(command, (err, stdout) => {
-      //           if (stdout) console.log('two ✅', stdout);
-      //         });
-      //       }
-      //     } else {
-      //       console.log('No changes to pull.');
-      //     }
-      //     resolve();
-      //   });
-      // });
+      await new Promise((resolve, reject) => {
+        git.fetch(async (err) => {
+          if (err) reject(err);
+
+          const data = await git.diff(['HEAD', 'origin/main']);
+          console.log(data);
+          // if (data) {
+          //   console.log('Changes detected. Pull is possible.');
+          //   for (const command of commands.inNeedPull) {
+          //     exec(command, (err, stdout) => {
+          //       if (stdout) console.log('two ✅', stdout);
+          //     });
+          //   }
+          // } else {
+          //   console.log('No changes to pull.');
+          // }
+          // resolve();
+        });
+      });
 
       // const status = await git.status();
       // if (!status.conflicted.length) {
