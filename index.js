1fd5e2a04a5f5858774bc768ced32740fcf55d53
MoamenWalid <momenelnager@gmail.com>
2024-05-06 17:27:04 +0300

    Changed files which need to commit: [index.js] in mk348ksw branch

--- index.js
+++ index.js
@@ -62,11 +62,6 @@ program.command('up')
       const status = await git.status();
       if (!status.conflicted.length) {
         console.log(`Not exist conflict ✅`);
-        // for (const command of commands.inAll) {
-        //   exec(command, (err, stdout) => {
-        //     if (stdout) console.log('Succes to merge data ✅', stdout);
-        //   });
-        // }
         exec(`git checkout ${mainBranch}`, (err, stdout) => {
           if (stdout) console.log('Succes to checkout main ✅', stdout);
         });
