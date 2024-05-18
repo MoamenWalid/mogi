7ba18a2705b2999a89312cf8590b9967511f89e3
MoamenWalid <momenelnager@gmail.com>
2024-05-18 15:30:40 +0300

    Changed files which need to commit: [index.js] in <sbxmn0lq> branch

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ async function mergePush(obj, mainBranch) {
 program
   .name('mogi')
   .description('GitHub Desktop simplifies Git and GitHub tasks by offering an intuitive interface, making it easy to upload all data without the need for manual upload commands.')
-  .version('1.6.0'); 
+  .version('1.7.0'); 
 
 program.command('up')
   .description('This base command to mogi start')
